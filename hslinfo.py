2668af5c5ea2e009a6fdd4c22dc482a87458a408
Teemu Kokkonen <teemu.on.ihminen@gmail.com>
2015-06-28 14:58:03 +0300

    Initial commit

---
+++ hslinfo.py
@@ -0,0 +1,137 @@
+import time
+import sys
+import json
+import os
+import collections
+
+from werkzeug.contrib.cache import SimpleCache
+from flask import Flask, render_template, Response
+import requests
+
+Stop = collections.namedtuple("Stop", ['code', 'max_entries'])
+Departure = collections.namedtuple("Departure", ['code_short', 'time', 'name'])
+
+try:
+    HSL_USERNAME = os.environ['HSL_USERNAME']
+    HSL_PASSWORD = os.environ['HSL_PASSWORD']
+except KeyError as e:
+    print "Missing configuration variable: %s" % e
+    print ""
+    print "You can get the API from:"
+    print "http://developer.reittiopas.fi/pages/en/account-request.php"
+    print "and then you can export it:"
+    print ""
+    print "export HSL_USERNAME=''"
+    print "export HSL_PASSWORD=''"
+    sys.exit(1)
+
+# Test if we have the right configuration
+example_request = requests.get(
+    'http://api.reittiopas.fi/hsl/prod/'
+    '?user=%s&pass=%s&request=stop&code=%s'
+    % (
+        HSL_USERNAME,
+        HSL_PASSWORD,
+        '2222209'
+    )
+)
+if example_request.status_code != 200:
+    print "HSL api returned error:"
+    print ""
+    print example_request.content
+    sys.exit(2)
+
+
+STOPS = (
+    Stop('2222209', 99),
+    # Stop('2222233', 3),
+    # Stop('2222235', 3),
+)
+API_CACHE = SimpleCache()
+
+
+def cache_get(url):
+    rv = API_CACHE.get(url)
+    if rv:
+        return rv
+    else:
+        rv = requests.get(url).json()
+        API_CACHE.set(url, rv, timeout=5*60)
+        return rv
+
+
+def get_line_name(find_code, response):
+    for line in response[0]['lines']:
+        code, name = line.split(":")
+        if code == find_code:
+            return name.split(',')[0]
+    return None
+
+
+def convertcode(code):
+    new_code = code[1:5]
+    if new_code[0] == '0':
+        new_code = new_code[1:]
+    return new_code.strip()
+
+
+def converttime(time):
+    time = str(time)
+    if len(time) == 3:
+        time = '0'+time
+    return '%s:%s' % (time[0:2], time[2:])
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def frontpage():
+    return render_template(
+        'index.html',
+        time=time.time()
+    )
+
+
+@app.route('/routes')
+def routes():
+    departures = {}
+    for stop in STOPS:
+        response = cache_get(
+            'http://api.reittiopas.fi/hsl/prod/'
+            '?user=%s&pass=%s&request=stop&code=%s'
+            % (
+                HSL_USERNAME,
+                HSL_PASSWORD,
+                stop.code
+            )
+        )
+        if response[0]['departures']:
+            departures[stop.code] = []
+            for i, departure in enumerate(response[0]['departures']):
+                if i >= stop.max_entries:
+                    break
+                departures[stop.code].append(
+                    Departure(
+                        convertcode(departure['code']),
+                        converttime(departure['time']),
+                        get_line_name(departure['code'], response)
+                    )
+                )
+
+    return render_template(
+        'routes.html',
+        departures=departures
+    )
+
+
+@app.route('/weather')
+def weather():
+    return Response(
+        json.dumps(
+            cache_get('http://api.openweathermap.org/data/2.5/weather?q=Espoo')
+        ),
+        mimetype='application/json'
+    )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=False)
---
+++ static/js/main.js
@@ -0,0 +1,148 @@
+$(function(){
+    var MORNING = [
+        'Good morning!',
+        'Morgen',
+        'Ni zao',
+        'Ohayo',
+        'Buenos dias',
+        'Bonjour',
+        'Hi sunshine, the earth says hi'
+    ];
+    var DAY = [
+        'Well hello!',
+        'Why hello there.',
+        'Hello',
+        'Yo.',
+        'Greetings!',
+    ];
+    var EVENING = DAY;
+    var NIGHT = [
+        'Good night',
+        'Gute nacht',
+        'Bonne nuit',
+        'Hey you!',
+        'Sweet sight, good night!',
+        'The day is over, the night has come.'
+    ]
+    var STILL_AWAKE = [
+        'Still awake?',
+        'Have sweet dreams',
+        'Huh, party hard?'
+    ]
+
+    var choice = function(arr) {
+        return arr[Math.round(Math.random() * (arr.length-1))];
+    };
+    var changeText = function($selector, arr) {
+        var lastChange = $selector.data('lastChange');
+        if (lastChange == null || (Date.now()-lastChange) > 3600*1000) {
+            console.log(lastChange);
+            $selector.data('lastChange', Date.now());
+            $selector.text(choice(arr));
+        }
+    };
+    var mod = function(n, m) {
+        return (n + m) % m;
+    }
+    var padding = function(str) {
+        if ((''+str).length == 1) return '0'+str;
+        else return str;
+    };
+    var updateTime = function(){
+        var date = new Date();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+        //hours = seconds % 24;
+
+        $('.slide-welcome').removeClass('morning morning10 day evening night supernight');
+        if (hours >= 6 && hours < 10) {
+            $('.slide-welcome').addClass('morning');
+            changeText($('.js-welcome-text'), MORNING);
+        }
+        else if (hours >= 10 && hours < 12) {
+            $('.slide-welcome').addClass('morning10');
+            changeText($('.js-welcome-text'), MORNING);
+        }
+        else if (hours >= 12 && hours < 17) {
+            $('.slide-welcome').addClass('day');
+            changeText($('.js-welcome-text'), DAY);
+        }
+        else if (hours >= 17 && hours < 20) {
+            $('.slide-welcome').addClass('evening');
+            changeText($('.js-welcome-text'), EVENING);
+        }
+        else if (hours >= 20 && hours < 24) {
+            $('.slide-welcome').addClass('night');
+            changeText($('.js-welcome-text'), NIGHT);
+        }
+        else {
+            $('.slide-welcome').addClass('supernight');
+            changeText($('.js-welcome-text'), STILL_AWAKE);
+        }
+
+        $('.js-clock').text(padding(hours) + ':' + padding(minutes) + ':' + padding(seconds));
+        $('.js-date').text(date.getDate()+'.'+(date.getMonth()+1)+'.'+date.getFullYear());
+        setTimeout(updateTime, 1000);
+    };
+    var updateWeather = function(){
+        $.getJSON(
+            '/weather'
+        ).success(function(data){
+            console.log();
+            var icon = data.weather[0].icon;
+            var temperature = Math.round((data.main.temp - 273.15)*10)/10;
+            $('.js-weather').text(temperature);
+            $('#js-weather-icon').attr(
+                'src',
+                'http://openweathermap.org/img/w/' + icon + '.png'
+            );
+            $('.js-weather-desc').text(
+                data.weather[0].main
+            )
+            setTimeout(function(){
+                updateWeather();
+            }, 1000*60*15);
+        }).error(function(err){
+            $('#update-container').text(err);
+        });
+    };
+
+    var updateRoutes = function(){
+        $('#update-container').load('/routes');
+        setTimeout(function(){
+            updateRoutes();
+        }, 1000*60*5);
+    };
+    var howOften = 20*1000;
+    var slideCounter = 0;
+    var lastSwitch = 0;
+    var updateSlides = function(){
+        lastSwitch = Date.now();
+        slideCounter += 1;
+        var i = slideCounter;
+        var slideCount = $('.slide').length
+        var previousSlide = mod(i-1, slideCount);
+        var activeSlide = i%slideCount;
+        $('.slide'+previousSlide).css('opacity', 0).css('z-index', 10);
+        $('.slide'+activeSlide).css('opacity', 1).css('z-index', 11);
+    };
+    $('html').on('click', function(e){
+        updateSlides();
+    });
+    setInterval(function(){
+        if ((Date.now() - lastSwitch) > howOften) {
+            lastSwitch = Date.now();
+            updateSlides();
+        }
+    }, 1000);
+
+    // TODO: ruokalistat
+
+
+    $('.slide').css('opacity', 0);
+    updateRoutes();
+    updateWeather();
+    updateTime();
+    updateSlides();
+});
